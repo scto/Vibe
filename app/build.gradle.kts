80110fee10a1eb2521a8b498b24b661a68bd3a22
scto <tschmid35@gmail.com>
2025-02-19 18:21:03 +0100

    first commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,7 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        
         vectorDrawables {
             useSupportLibrary = true
         }
@@ -24,7 +25,7 @@ android {
     
     signingConfigs {
         create("release") {
-            val propertiesFilePath = "/home/rohit/signing.properties"
+            val propertiesFilePath = "release-signing.properties"
             val propertiesFile = File(propertiesFilePath)
             
             if (propertiesFile.exists()) {
@@ -35,34 +36,62 @@ android {
                 storeFile = File(properties["storeFile"] as String)
                 storePassword = properties["storePassword"] as String
             } else {
-                println("Signing properties file not found at $propertiesFilePath")
+                println("Release Signing properties file not found at $propertiesFilePath")
+            }
+        }
+        create("debug") {
+            val propertiesFilePath = "debug-signing.properties"
+            val propertiesFile = File(propertiesFilePath)
+            
+            if (propertiesFile.exists()) {
+                val properties = Properties()
+                properties.load(propertiesFile.inputStream())
+                keyAlias = properties["keyAlias"] as String
+                keyPassword = properties["keyPassword"] as String
+                storeFile = File(properties["storeFile"] as String)
+                storePassword = properties["storePassword"] as String
+            } else {
+                println("Debug Signing properties file not found at $propertiesFilePath")
             }
         }
     }
 
     buildTypes {
+        debug {
+            isMinifyEnabled = true
+            //signingConfig = signingConfigs.getByName("release")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
         release {
             isMinifyEnabled = true
-            signingConfig = signingConfigs.getByName("release")
+            //signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
     }
+    
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    
     buildFeatures {
         compose = true
     }
+    
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
     }
+    
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -81,15 +110,18 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.runtime.ktx)
     implementation(libs.androidx.navigation.compose)
+    
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.coil.compose)
+    implementation(libs.material3)
+    
     testImplementation(libs.junit)
+    
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+    
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    implementation(libs.androidx.navigation.compose)
-    implementation(libs.coil.compose)
-    implementation(libs.material3)
-
 }
